faa9cbac1159bafe2482a237984766b00d50a1ef
Crystal Lyne~ <crystalrlyne@gmail.com>
2023-04-09 20:30:00 -0500

    student sign in

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <new-student-form>v-on:student-added="newStudentAdded"></new-student-form>
+    <student-message v-bind:student="mostRecentStudent"></student-message>
+    <student-table 
+    v-bind:students="students" 
+    v-on:students-arrived-or-left="studentsArrivedOrLeft"
+    v-on:delete-student="studentDeleted">
+    </student-table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentMessage from './components/StudentMessage.vue'
+import StudentTable from './components/StudentTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewStudentForm,
+    StudentMessage,
+    StudentTable
+  },
+  data() {
+    return {
+      students: [],
+      mostRecentStudent: {}
+    }
+  },
+  methods: {
+    newStudentAdded(student) {
+      this.students.push(student)
+      this.students.sort(function(s1, s2) {
+        return s1.name.toLowerCase() < s2.name.toLowerCase() ? -1 : 1
+      })
+    },
+    studentsArrivedOrLeft(student, present) {
+      // find student in array of students
+      //update present attribute
+
+      let updateStudent = this.students.find( function(s) {
+        if(s.name === student.name && s.starID === student.starID) {
+          return true // this is the student to update
+        }
+      })
+      if (updateStudent) {
+        updateStudent.present = present
+        this.mostRecentStudent = updateStudent
+      }
+    },
+    studentDeleted(student) {
+      // filter returns new array of all students for whom the function returns true
+      this.students = this.students.filter( function(s) {
+        if (s != student) {
+          return true
+        }
+      })
+
+      this.mostRecentStudent = {} // clear welcome message
+    }
   }
 }
 </script>
@@ -23,4 +73,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+@import "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css";
 </style>
